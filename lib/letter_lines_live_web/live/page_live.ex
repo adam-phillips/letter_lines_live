5a2b22fe126da9a08be07f1858423fc9e9159175
Adam Phillips <aadam21@gmail.com>
2020-08-05 18:50:55 -0400

    Initial LiveView Work (#3)
    Update view and template for LiveView. The game is now printing the
    initial BoardState, with hidden letters and the background
    differentiated.

--- lib/letter_lines_live_web/live/page_live.ex
+++ lib/letter_lines_live_web/live/page_live.ex
@@ -1,7 +1,7 @@
 defmodule LetterLinesLiveWeb.PageLive do
   use LetterLinesLiveWeb, :live_view
 
-  @impl true
+  @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
     game = LetterLinesElixir.generate_game()
 
@@ -28,15 +28,35 @@ defmodule LetterLinesLiveWeb.PageLive do
         ""
 
       :hidden ->
+        # @ expands variables to the same as `assigns.tile_size` -> this is a macro
+        # phx-value pushes the value into values available to `handle_event`
         ~E"""
-          <rect width="<% @tile_size %>" height="<%= @tile_size %>" class="hidden-letter-tile" />
+          <rect width="<%= @tile_size %>" height="<%= @tile_size %>" x="<%= tile_origin(assigns, x_index) %>"
+          y="<%= tile_origin(assigns, y_index) %>" class="hidden-letter-tile"
+          phx-click="tile_clicked" phx-value-x-index="<%= x_index %>" phx-value-y-index="<%= y_index %>" />
         """
 
       # Commented temporarily because the unused var error is annoying
       _letter ->
         ~E"""
-          <rect width="<% @tile_size %>" height="<%= @tile_size %>" class="exposed-letter-tile" />
+          <rect width="<%= @tile_size %>" height="<%= @tile_size %>" x="<%= tile_origin(assigns, x_index) %>" y="<%= tile_origin(assigns, y_index) %>" class="exposed-letter-tile" />
         """
     end
   end
+
+  def tile_origin(%{tile_size: tile_size, border_size: border_size}, index) do
+    (tile_size + border_size) * index + border_size
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("tile_clicked", values, socket) do
+    game = socket.assigns.game
+    IO.inspect(values)
+    socket = assign(socket, :game, game)
+
+    # TODO: get word that was clicked and set revealed true; function should be added to BoardState
+    {:noreply, socket}
+  end
+
+  # TODO add handle_event for button click - this will reveal word that contains the clicked letter
 end
