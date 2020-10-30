bd2b49a2adb04b4be41221276b8217a7b41e62db
Adam Phillips <aadam21@gmail.com>
2020-10-30 18:55:05 -0400

    Add a sample for the view test

--- test/letter_lines_live_web/live/page_live_test.exs
+++ test/letter_lines_live_web/live/page_live_test.exs
@@ -10,3 +10,11 @@ defmodule LetterLinesLiveWeb.PageLiveTest do
     assert render(page_live) =~ "Welcome to Phoenix!"
   end
 end
+
+#   1 2 3 4 5 6
+# 1 M C N N M
+# 2         X
+# 3   N M M L C
+# 4   X       M
+# 5   C       X
+# 6 X L M N C M
